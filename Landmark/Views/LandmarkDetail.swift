d833d91f4bf98376e4648d9ef4d1e09244384932
박인호 <i0n4h0o7@gmail.com>
2023-12-28 16:53:22 +0900

    (0.1.1) 랜드마크 리스트 생성

---
+++ Landmark/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmark
+//
+//  Created by 박인호 on 12/28/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
---
+++ Landmark/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmark
+//
+//  Created by 박인호 on 12/26/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
