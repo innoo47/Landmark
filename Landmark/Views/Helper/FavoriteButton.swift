248b4bf5083b66b2a187717ad92bf818c5322503
박인호 <i0n4h0o7@gmail.com>
2024-01-02 17:44:13 +0900

    (0.1.3) 즐겨찾기 토글 추가

---
+++ Landmark/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Landmark
+//
+//  Created by 박인호 on 1/2/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill": "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
